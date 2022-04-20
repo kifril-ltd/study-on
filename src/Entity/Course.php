646d9e99a7edb4637b9748f0d839c666f920627b
kifril-ltd <sukhorukov.kirill23@gmail.com>
2022-04-20 17:52:40 +0300

    Добавил ограничения для модели Курс

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -6,8 +6,13 @@ use App\Repository\CourseRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CourseRepository::class)]
+#[UniqueEntity(
+    fields: ['code'],
+    message: 'Данный код уже используется в системе',
+)]
 class Course
 {
     #[ORM\Id]
--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -4,17 +4,38 @@ namespace App\Form;
 
 use App\Entity\Course;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code')
-            ->add('name')
-            ->add('description')
+            ->add('code', TextType::class, [
+                'label' => 'Код курса',
+                'constraints' => [
+                    new NotBlank(message: 'Поле код курса является обязательным'),
+                    new Length(max: 255, maxMessage: 'Код курса должно быть не более 255 символов')
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Название',
+                'constraints' => [
+                    new NotBlank(message: 'Поле название является обязательным'),
+                    new Length(max: 255, maxMessage: 'Название должно быть не более 255 символов')
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Описание',
+                'constraints' => [
+                    new Length(max: 1000, maxMessage: 'Код курса должно быть не более 1000 символов')
+                ],
+            ])
         ;
     }
 
